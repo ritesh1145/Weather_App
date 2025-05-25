ceefa4c819a588fa05099a53d1f024856a400aa1
ritesh1145 <riteshs1145@gmail.com>
2025-05-25 13:33:58 +0530

    API update

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ xhr.addEventListener('readystatechange', function () {
 	}
 });
 
-xhr.open('GET', 'https://forecast9.p.rapidapi.com/');
+xhr.open('GET', 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle');
 xhr.setRequestHeader('x-rapidapi-key', '966b66366fmsheb36c2bf39b9088p1a3d94jsn85dac70b7362');
-xhr.setRequestHeader('x-rapidapi-host', 'forecast9.p.rapidapi.com');
+xhr.setRequestHeader('x-rapidapi-host', 'weather-by-api-ninjas.p.rapidapi.com');
 
 xhr.send(data);
